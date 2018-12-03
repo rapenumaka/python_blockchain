1318d26e1206f8f28dd111b6a0e1dfaae67f6aae
procode27 <invisible3d@outlook.com>
2018-12-02 21:24:52 -0600

    if loop python

---
+++ python_blockchain/if-example.py
@@ -0,0 +1,15 @@
+name = input()
+print(' You have entered name as '+name)
+address = input()
+
+print(' You live at this address '+address)
+
+if name == 'Mary':
+    print('Hello '+name)
+    if address == 'brentwood':
+        print('You live in brentwood')
+    else:
+        print(' You dont live in brentwood')
+
+
+
